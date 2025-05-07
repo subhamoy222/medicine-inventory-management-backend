c333506583f3bbd0459c79f8abf197c316ad7175
Subhamoy Sasmal <2023aspire117@gmail.com>
2025-05-07 19:32:28 +0530

    server.js file updated

--- server.js
+++ server.js
@@ -70,6 +70,7 @@ app.use((req, res, next) => {
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With, Accept');
   res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Expose-Headers', 'Content-Range, X-Content-Range');
   next();
 });
 
